51500ce0f8ea69fc7b8f531387769df1f0873597
Johannes Maria Frank <jmfrank63@gmail.com>
2018-08-02 16:08:51 +0100

    Simplyfied testing and settings to take just one file for the credentials

--- src/tests/test_settings.py
+++ src/tests/test_settings.py
@@ -1,33 +1,65 @@
 import os
 import pytest
 
+home_path = os.environ.get('HOME')
+FAKE_CREDENTIALS_PATH=os.path.join(home_path, '.box/settings.json')
+BASIC_FAKE_CREDENTIALS_PATH=os.path.join(home_path, '.box/basic_settings.json')
+
+FAKE_CREDENTIALS = u'''
+{
+    "boxAppSettings": {
+      "clientID": "qm07vgu6o41a86ct6k20wvj1dbhxtvwj",
+      "clientSecret": "FCIfOzIOPnRhsUXjQyNnwrxLAfqz8yYh",
+      "appAuth": {
+        "publicKeyID": "fdt7840b",
+        "privateKey": "-----BEGIN ENCRYPTED PRIVATE KEY-----\nMIIFDjBABgkqhkiG9w0BBQ0wMzAbBgkqhkiG9w0BBQwwDgQI9EBYmkRkTD8CAggA\nMBQGCCqGSIb3DQMHBAjeIF4owI/KSwSCBMjgFhV6p9aaWPV1IPwHARXe9W97cGdO\nzQsWnkkvHY2wGsvryxhdXddluNXUSWVLjUiF2GW8EEvyjYR5s+HjfqbTR5AgwofG\nuy1+BpREp66g3amftIypvrnFXXM4ig7+IGL+J9g70e5SA7yX7zUxEP/UefyATKyM\nZvIEbGsjJ5fcQbnSBd4rGcstuChTmrL3n9a5eQNyO3sobHHd0rZZ8xjWQ/jvHTeB\n0Yz0ZMqywDRDGKCSycyJNdyZbbl2YsZLu3zfhra2EFm9AkEZ6Erklws/4LZIbzZT\nMZfjYwukXRPHxtwt881nkXEbReFn3LwSqqUlzTeDXCsCK7ibvutM861a14+4VI4k\ninKXhigA+Q67y1wULEu2TJHxrZDuTiCHKNS7Q4RVU8AdpqtxXWhkJJCLVFT62fjf\n+7TlnrcTPV3F5rSDPoBVNjFXe7L7SxWAZvp0S4DD0K1TKlP9r5b4tAnG/Hhz0NHB\nAw3RyTUSLEl6Oi9kER+DwiYy4M+/2LVnnGqbGvHILf17n5bpVJvdBE/VIhAaQXlr\nhCQQhnSepkEJclWKTCEP7UTzMVV6rKG2JQBntB7H0HZ51h7Ty5LFg2cDtXDCgMle\nDtoVBn1wtAoYlk6hYFbCI5ZurGjWYXr13VGcM9psRKwWzi1Qd4Y0axv94aH9Mtwo\nvSO8E72pIr7/gyAHevLboGjU/xYkvOQv4/7yusyzePLPXocxuJP9bq/KKNNzp7Nw\npzeCWF1ezR30SNxeeM+wzXIr1qaFOIsQ3Qqp/Al9bF1crD/SYETpMvzXoVW808i0\nREBaZYTXwXqJTIyY1Q+so7QAvU33JM06ZClSRGN/Q8rXEwfkUGpe+mXf4684ZpDz\n9KKRkDqutyq0mU5LLLR0pFSNmDgV0uW0IY2663fPJFaCignbaSaLyDgsilPGoQgg\nv20POPAXY7LoiFoJ69IfTnXvFlGtjYlgphR/c8YuWjnmNVj6b7nsuACu986biucR\nCSuZdVgTg7K2H9ImPDll39M6VVgubkjIz5FP9DZqrKeKINVRwRUEKn/PTlmv0cAh\nHR66AB75crniVzv/1FxZ2LwjoHMilVbhiaXIyrft27/0/df3j7AAC8RjZ3JOfwJY\nLfvw4Nt9QQyAZ61vF2DEwONLknZ7+dsF4WPo32+Rb/YhV8tEqDS5wZCWZhKtAfyv\ncqGO3lmXSMeF7GOE5SaNNjM7xwZWoxTLGwdSlZrIjnsrPz3XHBNSbqbJW7l4KZMu\nQCqAlghJ+/XWQgDWnFZuNEM5rGpCMUuCK/RFUTySe2L+g+2ZhaEm2lKu+v2ep8rB\nyFiZgGZWvFojIqd3GrSCHJFM+LiCBahnH1unTJTnbZ1JdQV/QtzebszFPVA5uSY4\nQ/oRudpSPLPCegW7/jkdu+Mfm39nwZJN0m4aw1g6a3cEIbYdNFcsJuYYFhMYlY2U\n73SH98BJ1MlBejvZq/Ph5C45FK2cZNhpcJZkL/Jn/KMuk7hCHX18CEekXSpTv3KZ\ng9XRDneRx50/DdmSruGZR7Za2D90/KEtbFusn6iQnNhU8sy1BE0Rd+/2BNUNgD3N\n00Z7XZqy9SQvol9tW54L8E/JvRCum5ZG2J2wqUQBHRZB2cA0TVwQCei1+BBqBiex\nv0Q=\n-----END ENCRYPTED PRIVATE KEY-----\n",
+        "passphrase": "0e0c57a7daf7d0e30672f329b3270447"
+      }
+    },
+    "enterpriseID": "20675415",
+    "webhooks": {
+      "primaryKey": "viLEp36APtS20wIrOcrPQGSkW24oGvV8",
+      "secondaryKey": "G0EQpQZHQpcjRN9qQy1oLTTMIt5ljrOI"
+    }
+}
+'''
+BASIC_FAKE_CREDENTIALS= u'''
+{
+    "boxAppSettings": {
+      "clientID": "qm07vgu6o41a86ct6k20wvj1dbhxtvwj",
+      "clientSecret": "FCIfOzIOPnRhsUXjQyNnwrxLAfqz8yYh",
+      "appAuth": {
+        "publicKeyID": "",
+        "privateKey": "",
+        "passphrase": ""
+      }
+    },
+    "enterpriseID": "20675415" 
+}
+'''
+
 import src.utils.settings as settings
 
-@pytest.fixture
-def box_settings(fs):
-    return settings.Settings()
+
+def test_full_credentials_file_can_be_read(fs):
+    fs.CreateFile(FAKE_CREDENTIALS_PATH, contents=FAKE_CREDENTIALS)
+    box_settings = settings.Settings(FAKE_CREDENTIALS_PATH)
+    fake_credentials = FAKE_CREDENTIALS.replace('\n', '')
+    assert box_settings.client_id in fake_credentials
+    assert box_settings.client_secret in fake_credentials
+    assert box_settings.enterprise_id in fake_credentials
+    assert box_settings.public_key_id in fake_credentials
+    assert box_settings.private_key in fake_credentials
+    assert box_settings.passphrase in fake_credentials
+    assert box_settings.webhook_primary_key in fake_credentials
+    assert box_settings.webhook_secondary_key in fake_credentials
+
+def test_basic_credentials_file_can_be_read(fs):
+    fs.CreateFile(BASIC_FAKE_CREDENTIALS_PATH, contents=BASIC_FAKE_CREDENTIALS)
+    box_settings = settings.Settings(BASIC_FAKE_CREDENTIALS_PATH)
+    basic_fake_credentials = BASIC_FAKE_CREDENTIALS
+    assert box_settings.client_id in basic_fake_credentials
+    assert box_settings.client_secret in basic_fake_credentials
+    assert box_settings.enterprise_id in basic_fake_credentials
 
 
-def test_a_new_settings_instance_is_empty(box_settings):
-    assert isinstance(box_settings, settings.Settings)
-
-def test_settings_holds_a_valid_path_to_default_folder(fs, box_settings):
-    default_path = settings.DEFAULT_SETTINGS_PATH
-    box_settings.path = default_path
-    assert os.path.exists(default_path)
-
-def test_prompted_for_settings_if_empty_or_does_not_exist(capsys, monkeypatch):
-    def mock_input(instr):
-        print(instr, end='')
-        return settings.DEFAULT_CREDENTIALS
-    with monkeypatch.context() as mck:
-        mck.setattr('builtins.input', mock_input)
-        box_settings = settings.get_credentials()
-        out, _ = capsys.readouterr()
-        assert out == settings.DEFAULT_PROMPT
-        assert box_settings.credentials == settings.DEFAULT_CREDENTIALS
-
-
-
-def test_settings_file_can_be_read(fs):
-    pass
--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
+import json
 import os
 
-DEFAULT_SETTINGS_FOLDER = '.box'
-DEFAULT_CREDENTIALS_FILE = 'credentials.json'
-DEFAULT_SETTINGS_PATH = os.path.join(os.environ.get('HOME'), DEFAULT_SETTINGS_FOLDER)
-DEFAULT_CREDENTIALS = os.path.join(DEFAULT_SETTINGS_PATH, DEFAULT_CREDENTIALS_FILE)
-DEFAULT_PROMPT = 'Please enter path to credentials:'
-
-def get_credentials(credentials=DEFAULT_CREDENTIALS):
-    if os.path.exists(credentials):
-        return Settings(credentials)
-    credentials = input(DEFAULT_PROMPT)
-    return Settings(credentials)
+DEFAULT_SETTINGS_FOLDER_NAME = '.box'
+DEFAULT_CREDENTIALS_FILE_NAME = 'credentials.json'
+DEFAULT_SETTINGS_FOLDER_PATH = os.path.join(os.environ.get('HOME'), DEFAULT_SETTINGS_FOLDER_NAME)
+DEFAULT_CREDENTIALS_FILE_PATH = os.path.join(DEFAULT_SETTINGS_FOLDER_PATH, DEFAULT_CREDENTIALS_FILE_NAME)
 
 class Settings:
-    def __init__(self, credentials=None, *args, **kwargs):
-        if not credentials:
-            credentials = DEFAULT_CREDENTIALS
-        credentials_path, credentials_file= os.path.split(credentials)
-        if not os.path.exists(credentials_path):
-            os.makedirs(credentials_path, mode=0o775)
-        self.path = credentials_path
-        self.credentials = credentials
+    def __init__(self, credentials_file_path=DEFAULT_CREDENTIALS_FILE_PATH):
+        self.credentials_file_path = credentials_file_path
+        with open(self.credentials_file_path, 'r') as credentials_file:
+            self.credentials = json.loads(credentials_file.read().replace('\n', ''))
+        self.client_id = self.credentials['boxAppSettings']['clientID']
+        self.client_secret = self.credentials['boxAppSettings']['clientSecret']
+        self.enterprise_id = self.credentials['enterpriseID']
+        self.public_key_id = self.credentials['boxAppSettings']['appAuth']['publicKeyID']
+        self.private_key = self.credentials['boxAppSettings']['appAuth']['privateKey']
+        self.passphrase = self.credentials['boxAppSettings']['appAuth']['passphrase']
+        if 'webhooks' in self.credentials:
+            self.webhook_primary_key = self.credentials['webhooks']['primaryKey']
+            self.webhook_secondary_key = self.credentials['webhooks']['secondaryKey']
